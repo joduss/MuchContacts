7c2a80ea3ad20eea73a6076d9ab0a24bad8aa585
Jonathan Duss <jonathan.duss@epfl.ch>
2015-10-17 23:05:20 +0200

    - add image of phone - add Utility class

---
+++ Friendly/Utility.swift
@@ -0,0 +1,22 @@
+//
+//  StringExtension.swift
+//  MuchContacts
+//
+//  Created by Jonathan on 17/10/15.
+//  Copyright © 2015 ZaJo. All rights reserved.
+//
+
+import Foundation
+
+
+class Utility {
+    
+    
+    class func stringNotNilNotEmpty(s : String?) -> Bool {
+        return (s != nil && s != "")
+    }
+
+    
+    
+    
+}
